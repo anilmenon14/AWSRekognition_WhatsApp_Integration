40827b12a5435ee96d97afaa827dd2eef434c351
anilmenon14 <anil.menon14@gmail.com>
2020-09-21 17:43:53 +0400

    Basic integration for auto messaging response

--- handler.py
+++ handler.py
@@ -1,18 +1,22 @@
 import json
 
+from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingResponse
+from xml.etree import ElementTree
+
+
 
 def httpWebHooktoTwilioURL(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    response = MessagingResponse()
+    message = Message()
+    message.body('Hi Ani ! Ani Ani Ani')
+    response.append(message)
 
-    return response
+    return response.to_xml()
+
+#        "headers": {
+#            'Content-Type' : 'text/xml'
+#        },
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
